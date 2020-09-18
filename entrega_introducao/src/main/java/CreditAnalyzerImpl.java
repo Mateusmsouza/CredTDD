5f8b409ca1c8b3d464f1b3eec42b33fc6b069a1e
Mateus Souza <mateus_msouza@outlook.com>
2020-09-17 21:52:47 -0300

    finalizando implementação

--- entrega_introducao/src/main/java/CreditAnalyzerImpl.java
+++ entrega_introducao/src/main/java/CreditAnalyzerImpl.java
@@ -1,12 +1,19 @@
-public class CreditAnalyzerImpl implements CreditAnalyzer{
+import br.com.safeguard.check.SafeguardCheck;
+import br.com.safeguard.types.ParametroTipo;
 
+public class CreditAnalyzerImpl implements CreditAnalyzer{
 
     @Override
     public Boolean validateCreditAvailable(String userDocument, Integer userScore) {
-        if (userScore < 600){
-            return false;
+        Boolean creditAvailable = true;
+        Boolean validDocument = new SafeguardCheck().elementOf(userDocument, ParametroTipo.CPF_FORMATADO)
+                .validate()
+                .hasError();
+
+        if (userScore < 600) {
+            creditAvailable = false;
         }
 
-        return true;
+        return creditAvailable && validDocument;
     }
 }
